71ff80580161e3b44c1e17dc1c5f4466693a5a96
Cloud <smirnovceo@yandex.ru>
2021-11-19 17:27:42 +0500

    Refactor with exeptions, cleancode and readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # db_hack
 
-Script uses for import in shell to correct the django project information in DB
+The script is used to run in django-shell. Using functions from the script you can change some of the data in the database.
 
 ## Install 
 
@@ -9,39 +9,59 @@ Copy and add script.py near manage.py in the django project
 ## Get started
 
 Python3 and the django project must be installed with requirements, script is useless without this stuff.
+Move script.py near managept in the django project.
 
 Use this comand to start django-shell:
 ```
 python3 manage.py shell
 ```
-Copy and inter all script code into django-shell.
+Import script funcs in django-shell:
+```
+from script.py import *
+```
 Then you get possibilities to use script func in django-shell.
+
+You need to define the variable like, kid, it will contain the instance of the Schoolkid class, example:
+```
+schoolkid = get_schoolkid('Василий Пупкин', 'Б', 8)
+```
+Then you can use other functions.
 
 ## Funcs and args
 
-- `name_part` - string, part name of schoolkid you want to fix ifromation, like: 'Пупкин Василий'
-- `year_of_study` - integer, class year of this person
-- `letter_upper` - upper case string char of person group, like: 'A'
-- `point` - integer, mark in db 
+- `schoolkid` - the instance of the Schoolkid class, that contains in the variable you create befor. 
+- `full_name` - string, part name of schoolkid you want to fix ifromation, like: 'Пупкин Василий'
+- `study_year` - integer, class year of this person
+- `class_letter` - upper case string char of person group, like: 'A'
+- `bad_mark` - integer, mark in db 
 - `subject` - string, subject, like: 'Математика'
-- `text` - string, commendation, like: 'Хвалю!'
+- `commendation_text` - string, like: 'Хвалю!'
 - `lesson_number` - integer, number of specific lesson, default = None, then func will choose random lesson.
 
-`fix_marks(name_part, year_of_study, letter_upper, point)`
+Fix all bad marks less or equal `bad_mark` arg to best grade (default = 5) for one schoolkid:
+```
+fix_marks(schoolkid, bad_mark)
+```
 
-Fix all bad marks less or equal point arg to best grade (default - 5) for one schoolkid.
+Delete chastisements for schoolkid:
+```
+fix_chastisement(schoolkid)
+```
 
-`fix_chastisement(name_part, year_of_study, letter_upper)`
+Show count number of subject for schoolkid:
+```
+show_lessons_count(schoolkid, subject)
+```
 
-Delete chastisements for schoolkid.
+Create commendation for schoolkid:
+```
+create_commendation(schoolkid, subject, commendation_text, lesson_number)
+```
 
-`show_lessons_count(year_of_study, letter_upper, subject)`
-
-Show count number of subject for schoolkid.
-
-`create_commendation(part_name, year_of_study, letter_upper, subject, text, lesson_number)`
-
-Create commendation for schoolkid.
+If you whant to overrite the variable that you define before, use:
+```
+schoolkid = get_schoolkid(full_name, class_letter, study_year)
+```
 
 ## Project goals
 
--- script.py
+++ script.py
@@ -1,45 +1,60 @@
 import random
 
+from django.http import Http404
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
 
 
-def fix_marks(name_part, year_of_study, letter_upper, point):
-    schoolkid = Schoolkid.objects.get(
-        full_name__contains=name_part, group_letter=letter_upper, year_of_study=year_of_study)
+def get_schoolkid(full_name, class_letter, study_year):
+    try:
+        return Schoolkid.objects.get(
+            full_name__contains=full_name,
+            group_letter=class_letter.upper(),
+            year_of_study=study_year
+        )
+    except ObjectDoesNotExist:
+        raise Http404('No matches in the db')
+    except MultipleObjectsReturned:
+        raise Http404('Find more than one schoolkid')
+
+
+def fix_marks(schoolkid, bad_mark):
     bad_marks = Mark.objects.filter(
-        schoolkid=schoolkid, points__lte=point)
+        schoolkid=schoolkid, points__lte=bad_mark)
     for mark in bad_marks:
         mark.points = 5
         mark.save()
 
 
-def fix_chastisement(name_part, year_of_study, letter_upper):
-    schoolkid = Schoolkid.objects.get(
-        full_name__contains=name_part, group_letter=letter_upper, year_of_study=year_of_study)
+def fix_chastisement(schoolkid):
     chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
     chastisements.delete()
 
 
-def show_lessons_count(year_of_study, letter_upper, subject):
+def show_lessons_count(schoolkid, subject):
     lessons = Lesson.objects.filter(
-        year_of_study=year_of_study, group_letter=letter_upper, subject__title=subject).order_by('date')
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter.upper(),
+        subject__title=subject
+    ).order_by('date')
+
     print(lessons.count())
 
 
-def create_commendation(part_name, year_of_study, letter_upper, subject, text, lesson_number=None):
-    schoolkid = Schoolkid.objects.get(
-        full_name__contains=part_name, year_of_study=year_of_study, group_letter=letter_upper)
+def create_commendation(schoolkid, subject, commendation_text):
     lessons = Lesson.objects.filter(
-        year_of_study=year_of_study, group_letter=letter_upper, subject__title=subject).order_by('date')
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter.upper(),
+        subject__title=subject
+    ).order_by('date')
 
-    if lesson_number:
-        lesson = lessons[lesson_number]
-    else:
-        lesson = random.choice(lessons)
-
-    teacher = lesson.teacher
-    subject = lesson.subject
-    date = lesson.date
+    lesson = random.choice(lessons)
 
     Commendation.objects.create(
-        schoolkid=schoolkid, teacher=teacher, subject=subject, created=date, text=text)
+        schoolkid=schoolkid,
+        teacher=lesson.teacher,
+        subject=lesson.subject,
+        created=lesson.date,
+        text=commendation_text
+    )
